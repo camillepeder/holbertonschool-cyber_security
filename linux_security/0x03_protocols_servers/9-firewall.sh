7d72ea86e251481c603cdea56d025c7981e2175b
camillepeder <gonzalopeder2@gmail.com>
2024-10-18 20:18:35 -0400

    Task 9 corrected

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo iptables -A INPUT -i lo -p tcp --dport 22 -j ACCEPT
-sudo iptables -A OUTPUT -i lo -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -i lo -p tcp --dport ssh -j ACCEPT
+sudo iptables -A OUTPUT -i lo -p tcp --dport ssh -j ACCEPT
