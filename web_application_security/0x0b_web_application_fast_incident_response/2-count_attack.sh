12b304881b2ae38cb6b51424a49abcdfbdc1baf2
camillepeder <gonzalopeder2@gmail.com>
2024-11-20 10:12:43 -0500

    Task 2 correction

--- web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/2-count_attack.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n +23 logs.txt | awk '{print $1}' | uniq -c | sort -nr | head -n 1 | awk '{print $1}'
+tail -n +23 logs.txt | awk '{print $1}'| sort -nr | uniq -c | sort -nr | head -n 1 | awk '{print $1}'
