95f127821e144917ea426eac947649704950174c
camillepeder <gonzalopeder2@gmail.com>
2024-11-24 15:36:16 -0500

    Task 5 corrected

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep 'new user' auth.log | awk '{print $8}' | awk -F'[=,]' '{print $2}' | tr '\n' ',' | sed 's/,$/\n/'
+grep 'new user' auth.log | awk '{print $8}' | sort | awk -F'[=,]' '{print $2}' | tr '\n' ',' | sed 's/,$/\n/'
