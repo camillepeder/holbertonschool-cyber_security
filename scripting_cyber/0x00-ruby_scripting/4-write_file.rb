9d190c828f33dce1830231be2420a34da1772672
gonzalopedernera <gonzalopeder2@gmail.com>
2024-12-15 19:02:05 +0000

    Task 4 correction

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -1,8 +1,13 @@
 #!/usr/bin/env ruby
 require 'json'
 def merge_json_files(file1_path, file2_path)
-    file_data = File.read(file1_path)
-    json_data = JSON.parse(file_data)
+    file1_data = File.read(file1_path)
+    json1_data = JSON.parse(file1_data)
 
-    File.write(file2_path, JSON.dump(json_data))
+    file2_data = File.read(file2_path)
+    json2_data = JSON.parse(file2_data)
+
+    merged_data = json1_data + json2_data
+ 
+    File.write(file2_path, JSON.pretty_generate(merged_data))
 end
